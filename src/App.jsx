dc0ba98f4b61615e3818283f50ea0dcb925be94c
ryanluo <zhengyangluo180@gmail.com>
2025-02-16 18:53:04 +1100

    adding another button, trying to achieve function of upload image locally, adding text to the image and then download it as a whole. should be able to achieve completely using frontend only

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./component/Header/Header";
 import InputBox from "./component/Input-box/Input-box";
+import OpenImageFromLocalButton from "./component/open-image-from-local-button/open-image-from-local-button";
 import RequestNewImageButton from "./component/Request-new-image-button/Request-new-image-button";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <InputBox />
       <RequestNewImageButton />
+      <OpenImageFromLocalButton />
     </>
   );
 }
